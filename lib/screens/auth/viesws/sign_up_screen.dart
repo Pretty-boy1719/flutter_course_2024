973d7ac263b1ad80ac8e1233f08d9ad3bdba54c1
Pretty-Boy <kirill.kiselev.04@list.ru>
2024-12-26 23:08:56 +0300

    Authentication

---
+++ lib/screens/auth/viesws/sign_up_screen.dart
@@ -0,0 +1,169 @@
+import 'package:first_stuff/components/template_text_field.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_repository/user_repository.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../blocs/sign_up_bloc/sign_up_bloc.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final passwordController = TextEditingController();
+  final emailController = TextEditingController();
+  final nameController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  IconData iconPassword = CupertinoIcons.eye_fill;
+  bool obscurePassword = true;
+  bool signUpRequired = false;
+
+  IconData iconPasswordSufix = Icons.remove_red_eye;
+  IconData iconPasswordPrefix = Icons.password_outlined;
+  IconData iconEmail = Icons.email;
+  IconData iconEye = Icons.remove_red_eye;
+  IconData iconEyeEmpty = Icons.remove_red_eye_outlined;
+  IconData iconPerson = Icons.person;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<SignUpBloc, SignUpState>(
+      listener: (context, state) {
+        if (state is SignUpSuccess) {
+          setState(() {
+            signUpRequired = false;
+          });
+        } else if (state is SignUpProcess) {
+          setState(() {
+            signUpRequired = true;
+          });
+        } else if (state is SignUpFailure) {
+          return;
+        }
+      },
+      child: Form(
+        key: _formKey,
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              //Email
+              SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: TemplateTextField(
+                      controller: emailController,
+                      hintText: 'Email',
+                      obscureText: false,
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (val) {
+                        if (val!.isEmpty) {
+                          return 'Please fill in this field';
+                        } else if (!RegExp(r'^[\w-\.]+@([\w-]+.)+[\w-]{2,4}$')
+                            .hasMatch(val)) {
+                          return 'Please enter a valid email';
+                        }
+                        return null;
+                      })),
+              const SizedBox(height: 10),
+              //input password
+              const SizedBox(height: 10),
+              //Password
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: TemplateTextField(
+                    controller: passwordController,
+                    hintText: 'Password',
+                    obscureText: obscurePassword,
+                    keyboardType: TextInputType.visiblePassword,
+                    prefixIcon: Icon(iconPasswordPrefix),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          obscurePassword = !obscurePassword;
+                          if (obscurePassword) {
+                            iconPasswordSufix = iconEye;
+                          } else {
+                            iconPasswordSufix = iconEyeEmpty;
+                          }
+                        });
+                      },
+                      icon: Icon(iconPasswordSufix),
+                    ),
+                    validator: (val) {
+                      if (val!.isEmpty) {
+                        return 'Please fill in this field';
+                      } else if (!RegExp(
+                              r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~`)\%\-(_+=;:,.<>/?"[{\]}\|^]).{8,}$')
+                          .hasMatch(val)) {
+                        return 'Please enter a valid password';
+                      }
+                      return null;
+                    }),
+              ),
+              const SizedBox(height: 10),
+              //Name of person
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: TemplateTextField(
+                    controller: nameController,
+                    hintText: 'Name',
+                    obscureText: false,
+                    keyboardType: TextInputType.name,
+                    prefixIcon: Icon(iconPerson),
+                    validator: (val) {
+                      if (val!.isEmpty) {
+                        return 'Please fill in this field';
+                      } else if (val.length > 30) {
+                        return 'Name too long';
+                      }
+                      return null;
+                    }),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+              !signUpRequired
+                  ? SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: TextButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              MyUser myUser = MyUser.empty;
+                              myUser.email = emailController.text;
+                              myUser.name = nameController.text;
+
+                              setState(() {
+                                context.read<SignUpBloc>().add(SignUpRequired(
+                                    myUser, passwordController.text));
+                              });
+                            }
+                          },
+                          style: TextButton.styleFrom(
+                              elevation: 3.0,
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.primary,
+                              foregroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(60))),
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 25, vertical: 5),
+                            child: Text(
+                              'Sign Up',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          )),
+                    )
+                  : const CircularProgressIndicator()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
